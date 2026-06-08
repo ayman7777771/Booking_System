4a26d67dfab113b311e9bb34922f7a5fc74b1679
ayman <aymane27@gmail.com>
2026-06-08 19:50:53 +0100

    take remote

--- routes/web.php
+++ routes/web.php
@@ -5,12 +5,13 @@ use App\Http\Controllers\Client\ReservationController;
 use App\Http\Controllers\Client\ReviewController;
 use App\Http\Controllers\MessageController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Provider\PhotoController;
+use App\Http\Controllers\Provider\PlanningController;
 use App\Http\Controllers\Provider\ProviderController;
 use App\Http\Controllers\Provider\ServiceController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-// 1. Home Page
 Route::get('/', function () {
     return Inertia::render('Welcome', [
         'canLogin' => Route::has('login'),
@@ -18,20 +19,15 @@ Route::get('/', function () {
     ]);
 });
 
-// 2. Dashboard - يعرض جميع الخدمات والمقدمين (للـ Client)
 Route::get('/provider/profile/{provider}', [ProviderController::class, 'profile']);
 
-// 2. Dashboard - يعرض جميع الخدمات للـ Client
 Route::get('/dashboard', [ProviderController::class, 'index'])->name('dashboard');
 
-// 3. Routes للـ Auth
 Route::middleware('auth')->group(function () {
-    // Profile
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-    // Client - Reservations & Reviews
     Route::get('/bookings', [ReservationController::class, 'index'])->name('reservations.index');
     Route::post('/reservations', [ReservationController::class, 'store'])->name('reservations.store');
 
@@ -53,17 +49,20 @@ Route::middleware(['auth', 'admin'])
         Route::delete('/users/{user}', [AdminDashboardController::class, 'destroyUser'])->name('users.destroy');
     });
 
-// 4. Routes للـ Provider
-Route::middleware(['auth'])
+Route::middleware(['auth', 'provider'])
     ->prefix('provider')
     ->name('provider.')
     ->group(function () {
         Route::get('/Dashboard', [ProviderController::class, 'dashboard'])->name('dashboard');
         Route::patch('/profile/{provider}', [ProviderController::class, 'update'])->name('profile.update');
-        Route::resource('services', ServiceController::class)->only(['index', 'create', 'store', 'update', 'destroy']);
+        Route::post('/plannings', [PlanningController::class, 'store'])->name('plannings.store');
+        Route::post('/services/{service}/photos', [PhotoController::class, 'store'])->name('services.photos.store');
+        Route::delete('/photos/{photo}', [PhotoController::class, 'destroy'])->name('photos.destroy');
+        Route::patch('/reservations/{reservation}/accept', [ReservationController::class, 'accept'])->name('reservations.accept');
+        Route::patch('/reservations/{reservation}/refuse', [ReservationController::class, 'refuse'])->name('reservations.refuse');
+        Route::resource('services', ServiceController::class)->only(['create', 'store', 'update', 'destroy']);
     });
 
-// Route لعرض خدمات مقدم معين
 Route::get('/provider/{id}/services', [ServiceController::class, 'byProvider'])->name('services.byProvider');
 Route::get('/provider/profile/{provider}', [ProviderController::class, 'profile'])->name('provider.profile');
 
